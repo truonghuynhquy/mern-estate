2ca8346c559a12544db0e37c72f6265b64702c67
Truong Huynh Quy <quytruong2509@gmail.com>
2024-05-22 09:41:32 +0700

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import morgan from "morgan";
+
+import { checkMySQLConnection } from "./database/dbConnection.js";
 
 const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
 
+checkMySQLConnection();
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
