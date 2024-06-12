1f58073243eae99dd923da0cb3ae86dede61322b
Truong Huynh Quy <quytruong2509@gmail.com>
2024-06-12 18:50:29 +0700

    Add: Firebase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,6 +1,15 @@
 export default function OAuth() {
+  const handleGoogleClick = () => {
+    try {
+      console.log("");
+    } catch (error) {
+      console.log("Could not sign in with Google", error);
+    }
+  };
+
   return (
     <button
+      onClick={handleGoogleClick}
       type="button"
       className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
     >
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnL5KLECwN3fKCxIB-TVjfUd2RN5C5pUU",
+  authDomain: "mern-estate-38b68.firebaseapp.com",
+  projectId: "mern-estate-38b68",
+  storageBucket: "mern-estate-38b68.appspot.com",
+  messagingSenderId: "98560234318",
+  appId: "1:98560234318:web:02165347cdab6e9a80841f",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
