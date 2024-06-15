d123f89e29ad4bd3f86e8685a30834815adecd39
Truong Huynh Quy <quytruong2509@gmail.com>
2024-06-15 11:54:29 +0700

    Add: Complete the UI Profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,58 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 export default function Profile() {
-    return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const { loading } = useSelector((state) => state.error);
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+
+      <form action="" className="flex flex-col gap-4">
+        <img
+          src={currentUser.data.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          defaultValue={currentUser.data.username}
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          defaultValue={currentUser.data.email}
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          Update
+        </button>
+        <Link
+          to="/create-listing"
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+        >
+          Create Listing
+        </Link>
+      </form>
+
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 }
