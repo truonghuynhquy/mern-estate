b1797afa34eaabe1a80ca29962a2380b11b1016f
Truong Huynh Quy <quytruong2509@gmail.com>
2024-06-17 18:25:40 +0700

    Add: Update Listing UI complete

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -51,3 +51,55 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const { id, ...rest } = req.body;
+  const newListing = { ...rest, id };
+
+  const { userRef, created_at, updated_at, ...result } = newListing;
+
+  try {
+    const listingRedis = await redis.get(`listing:${req.params.id}`);
+    const idUser = JSON.parse(listingRedis).userRef;
+
+    if (req.user.id !== +idUser) {
+      return next(new AppError("You can only delete your own listings!", 401));
+    }
+    const valuesArray = Object.values({
+      ...result,
+      imageUrls: JSON.stringify(result.imageUrls),
+    });
+
+    const updateListing = await dbQuery.query(
+      "UPDATE listings SET name =?, description =?, address =?, regularPrice =?, discountPrice =?, bathrooms =?, bedrooms =?, furnished =?, parking =?, type =?, offer =?, imageUrls =? WHERE id =?",
+      valuesArray
+    );
+    if (updateListing.affectedRows === 0) {
+      return next(new AppError("Listing not found", 404));
+    }
+    const data = await dbQuery.query("SELECT * FROM listings WHERE id =?", [
+      req.params.id,
+    ]);
+    await redis.set(`listing:${req.params.id}`, JSON.stringify(data[0]));
+    res.json({ success: true, data: data[0] });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) => {
+  try {
+    let data = await redis.get(`listing:${req.params.id}`);
+    if (!data) {
+      data = await dbQuery.query("SELECT * FROM listings WHERE id = ?", [
+        req.params.id,
+      ]);
+      await redis.set(`listing:${req.params.id}`, JSON.stringify(data[0]));
+    } else {
+      data = JSON.parse(data);
+    }
+    res.json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -3,11 +3,15 @@ import { verifyToken } from "../utils/verifyToken.js";
 import {
   createListing,
   deleteListing,
+  updateListing,
+  getListing,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", verifyToken, getListing);
 
 export default router;
